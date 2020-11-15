1d9fdf2918c54f56f0b082b342cc0d1009917b65
Ivor Scott <ivor@devpie.io>
2020-11-15 20:12:00 +0100

    feat: created base listener and publisher for typescript package

---
+++ src/base-publisher.ts
@@ -0,0 +1,31 @@
+import { Stan } from 'node-nats-streaming';
+import { Events, Commands } from '.';
+
+type Subjects = Events | Commands
+
+interface Event {
+    subject: Subjects;
+    data: any;
+}
+
+export abstract class Publisher<T extends Event> {
+    abstract subject: T['subject'];
+    private client: Stan;
+
+    constructor(client: Stan) {
+        this.client = client
+    }
+
+    publish(data: T['data']) {
+        return new Promise((resolve,reject)=> {
+            this.client.publish(this.subject, JSON.stringify(data), (err)=> {
+               if (err) {
+                   return reject(err)
+               }
+               resolve();
+                console.log('Event Published', this.subject)
+            })
+        })
+       
+    }
+}
