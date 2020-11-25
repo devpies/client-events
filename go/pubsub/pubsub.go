b980b03711d811ed2589c45099a609897cc3f58b
Ivor Scott <ivor@devpie.io>
2020-11-25 20:13:20 +0100

    feat: create wrapper around go client for NATS Streaming

---
+++ go/pubsub/pubsub.go
@@ -0,0 +1,79 @@
+package pubsub
+
+import (
+	"fmt"
+	"github.com/nats-io/stan.go"
+	"log"
+	"sync"
+	"time"
+)
+
+// Client
+type Client struct {
+	Conn stan.Conn
+}
+
+func NewClient(clusterId, clientId, url string) *Client {
+	sc, err := stan.Connect(clusterId, clientId, stan.NatsURL(url),
+		stan.SetConnectionLostHandler(func(_ stan.Conn, reason error) {
+			log.Fatalf("Connection failed for some reason: %v", reason)
+		}))
+	if err != nil {
+		fmt.Printf("error: %v",err)
+	}
+	defer sc.Close()
+
+	return &Client{
+		Conn: sc,
+	}
+}
+
+func (c *Client) Listen(subj, quegrp string, parseMsg func(msg *stan.Msg), opts ...stan.SubscriptionOption) {
+	sub, err := c.Conn.QueueSubscribe(subj, quegrp, parseMsg, opts...)
+	if err != nil {
+		c.Conn.Close()
+		log.Fatal(err)
+	}
+	sub.Unsubscribe()
+}
+
+
+
+func (c *Client) Publish(sub string, msg []byte) {
+	ch := make(chan bool)
+	var glock sync.Mutex
+	var guid string
+
+	// Ack Handler
+	acb := func(lguid string, err error) {
+		glock.Lock()
+		log.Printf("Received ACK for guid %s\n", lguid)
+		defer glock.Unlock()
+		if err != nil {
+			log.Fatalf("Error in server ack for guid %s: %v\n", lguid, err)
+		}
+		if lguid != guid {
+			log.Fatalf("Expected a matching guid in ack callback, got %s vs %s\n", lguid, guid)
+		}
+		ch <- true
+	}
+
+	// Publish message
+	glock.Lock()
+	guid, err := c.Conn.PublishAsync(sub, msg, acb)
+	if err != nil {
+		log.Fatalf("Error during async publish: %v\n", err)
+	}
+	glock.Unlock()
+	if guid == "" {
+		log.Fatal("Expected non-empty guid to be returned.")
+	}
+	log.Printf("Published [%s] : '%s' [guid: %s]\n", sub, msg, guid)
+
+	select {
+	case <-ch:
+		break
+	case <-time.After(5 * time.Second):
+		log.Fatal("timeout")
+	}
+}
