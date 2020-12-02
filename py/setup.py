05efbf1e4e4584650f196ddce6e851b6cfa52ee9
Ivor Scott <ivor@devpie.io>
2020-12-02 23:33:04 +0100

    feat: update versions

--- py/setup.py
+++ py/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="devpie-client-events",
-    version="0.0.17",
+    version="0.0.23",
     author="ivorscott",
     author_email="ivor@devpie.io",
     long_description=long_description,
