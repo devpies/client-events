88ff131d31b5b4ae9c2b28a8a2f172841aaf2163
Ivor Scott <ivor@devpie.io>
2021-04-17 19:36:32 +0200

    fix: export cmd listener

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
-export * from './events';
-export { Listener } from './base-listener';
-export { Publisher } from './base-publisher';
+export * from "./events";
+export { Listener } from "./base-listener";
+export { Publisher } from "./base-publisher";
+export { CommandListener } from "./base-cmd-listener";
